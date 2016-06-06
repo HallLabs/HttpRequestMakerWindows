adb6dc334b2b0399e2c3b28752d72bdce2efb714
Taylor Robbins <robbitay@gmail.com>
2016-06-06 13:06:25 -0600

    Changed History View to Consolas

--- HttpRequestMaker/Form1.Designer.cs
+++ HttpRequestMaker/Form1.Designer.cs
@@ -544,6 +544,7 @@
             this.HistoryListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.HistoryListBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HistoryListBox.FormattingEnabled = true;
             this.HistoryListBox.Location = new System.Drawing.Point(0, 0);
             this.HistoryListBox.Name = "HistoryListBox";
